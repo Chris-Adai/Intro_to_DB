d5fac2b4a9c5e5dbedb5d9620fc45532259ac6d6
Chris-Adai <christopheradai.ca@gmail.com>
2025-02-06 05:24:30 -0800

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,5 @@
 INSERT INTO Customers (customer_id, customer_name, email, address) 
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+INSERT INTO customer (customer_id, name, email, phone)  
+VALUES (1, 'John Doe', 'johndoe@example.com', '1234567890');
